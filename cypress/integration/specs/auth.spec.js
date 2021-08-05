5961109a3feae9609e7330fbf4dbb446dd78ccc2
Antonio Crespo <lic.crespoantonio@gmail.com>
2021-08-04 22:43:49 -0300

    add some validations

--- cypress/integration/specs/auth.spec.js
+++ cypress/integration/specs/auth.spec.js
@@ -1,22 +1,64 @@
-it('Returns 200 when we hit /register', ()=>{
-    let body = {
-        name:'testName',
-        email:'foo@bar.com',
-        password:'Test'
-    }
 
-    cy.request('POST', 'http://localhost:3000/api/user/register', body)
-    .then((response)=>{
-        expect(response.status).to.eq(200);
+describe('/user/register', ()=>{
+    const registerEndpoint = 'http://localhost:3000/api/user/register'
+    it('create user with valid body', ()=>{
+        let body = {
+            name:'testName',
+            email:'foo@bar.com',
+            password:'Test1234'
+        }
+    
+        cy.request('POST', registerEndpoint, body)
+        .then((response)=>{
+            expect(response.status).to.eq(200);
+            expect(response.body.name).to.eq('testName');
+            expect(response.body.email).to.eq('foo@bar.com');
+            expect(response.body.password).to.eq('Test1234');
+    
+        })
     })
-})
+    
+    it('doesnt allow user creation with bad user body', ()=>{
+        let badTestUser = {
+            name:'1',
+            email:'foo',
+            password:'1'
+        }
+        cy.request({
+            method: 'POST',
+            url: registerEndpoint,
+            failOnStatusCode: false,
+            body: badTestUser
+        }).then((response)=>{
+            expect(response.status).to.eq(400);
+        })
+    })
 
-it('Returns 400 when we hit /register with no body', ()=>{
-    cy.request({
-        method: 'POST',
-        url: 'http://localhost:3000/api/user/register',
-        failOnStatusCode: false,
-    }).then((response)=>{
-        expect(response.status).to.eq(400);
+    it('doesnt allow user creation with invalid email', ()=>{
+        let badTestUser = {
+            name:'validUser',
+            email:'invalid',
+            password:'validPassword'
+        }
+        cy.request({
+            method: 'POST',
+            url: registerEndpoint,
+            failOnStatusCode: false,
+            body: badTestUser
+        }).then((response)=>{
+            expect(response.status).to.eq(400);
+            expect(response.body).to.eq('"email" must be a valid email');
+        })
+    })
+
+
+    it('Returns 400 when we hit with no body', ()=>{
+        cy.request({
+            method: 'POST',
+            url: registerEndpoint,
+            failOnStatusCode: false,
+        }).then((response)=>{
+            expect(response.status).to.eq(400);
+        })
     })
 })
